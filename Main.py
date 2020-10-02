39e6c2c11a4dadf4d0316e92a3d98080bbc79827
Gaurav <gauarvsharma.8699@gmail.com>
2020-10-03 01:35:27 +0530

    modified join

--- Main.py
+++ Main.py
@@ -4,9 +4,7 @@ import numpy as np
 import pandas as pd
 from preprocessing import Preprocess
 
-'''
 df = []
-i = 0
 for grade in range(5):
     images=[ cv2.imread(file) for file in glob.glob(r'C:/Users/Gaurav/Desktop/Minor_Project/MinorProject/dataset/train/'+str(grade)+'/*.png')]
     path_input = r'C:/Users/Gaurav/Desktop/Minor_Project/MinorProject/dataset/train/'+str(grade)
@@ -23,13 +21,20 @@ for grade in range(5):
             cv2.imwrite(r'C:/Users/Gaurav/Desktop/Minor_Project/MinorProject/dataset/train/'+str(grade)+'/'+w+'.png', np.fliplr(med))
         else:
             cv2.imwrite(r'C:/Users/Gaurav/Desktop/Minor_Project/MinorProject/dataset/train/'+str(grade)+'/'+w+'.png', med)
-        df.append([img_pre,grade+1])
-    #print(len(df))
+        
+        #img_pre:grayScale->roi->CLAHE->edgeDetection->contour
+        #img_CLAHE:grayScale->CLAHE
+        df.append([img_CLAHE,grade+1])
+
 df = pd.DataFrame(df,columns = ['Image','Grade'])
-print(df.head())
+#shuffle
+df = df.sample(frac = 1)
+
+#df has two coloumns Image and Grade-> split it(70,30) and apply model
+#don't paste the code directly rather make a different .py file and use functions
+
 '''
-
-
+#testing Code
 img = cv2.imread("2.png")
 if img is None:
     print("Error: Image Doesn't Exist")
@@ -42,4 +47,4 @@ img_pre,img_CLAHE = Preprocess(img1)    #this is a numpy Array
 #work from here 
 cv2.imwrite("2_preprocessed.jpg", img_pre)
 cv2.imwrite("2_CLAHE.jpg", img_CLAHE)
-
+'''
