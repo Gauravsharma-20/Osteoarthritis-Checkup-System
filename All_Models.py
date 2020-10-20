22252f109399c683f4976a9cd1831b5e60679d5a
Gaurav <gauarvsharma.8699@gmail.com>
2020-10-20 11:05:41 +0530

    All models Tried - overfitting and lack of regularisation

---
+++ All_Models.py
@@ -0,0 +1,84 @@
+import numpy as np 
+import pandas as pd
+import keras
+import tensorflow as tf
+'''
+from keras.models import Sequential
+from keras.optimizers import Adam
+from keras.losses import categorical_crossentropy
+from keras.layers import Dense, Flatten, Conv3D, MaxPooling3D, DropoutDense, Flatten, Conv2D, MaxPooling2D, Dropout,GlobalAveragePooling2D,BatchNormalization
+from sklearn.model_selection import train_test_split
+'''
+'''Takes the Input Shape and returns the model trained over respective dataset'''
+def ConvPool_CNN_C(inputShape):
+  model = Sequential()
+  model.add(Conv2D(96,kernel_size=(3,3),activation='relu',padding='same'))
+  model.add(Conv2D(96,kernel_size=(3,3),activation='relu',padding='same'))
+  model.add(Conv2D(96,kernel_size=(3,3),activation='relu',padding='same'))
+  model.add(MaxPooling2D(pool_size=(3,3),strides=2))
+  model.add(Conv2D(192,(3,3),activation='relu',padding='same'))
+  model.add(Conv2D(192,(3,3),activation='relu',padding='same'))
+  model.add(Conv2D(192,(3,3),activation='relu',padding='same'))
+  model.add(MaxPooling2D(pool_size=(3,3),strides=2))
+  model.add(Conv2D(192,(3,3),activation='relu',padding='same'))
+  model.add(Conv2D(192,(1,1),activation='relu'))
+  model.add(Conv2D(5,(1,1)))
+  model.add(GlobalAveragePooling2D())
+  model.add(Flatten())
+  model.add(Dense(5, activation='softmax'))
+  model.build(inputShape)
+  model.compile(loss=categorical_crossentropy,optimizer=keras.optimizers.Adam(0.001),metrics=['accuracy'])
+  return model
+
+def all_cnn_c(inputShape):
+  model = Sequential()
+  model.add(Conv2D(96,kernel_size=(3,3),activation='relu',padding='same'))
+  model.add(Conv2D(96,kernel_size=(3,3),activation='relu',padding='same'))
+  model.add(Conv2D(96,kernel_size=(3,3),activation='relu',padding='same'))
+  model.add(Conv2D(192,(3,3),activation='relu',padding='same'))
+  model.add(Conv2D(192,(3,3),activation='relu',padding='same'))
+  model.add(Conv2D(192,(3,3),activation='relu',padding='same'))
+  model.add(Conv2D(192,(3,3),activation='relu',padding='same'))
+  model.add(Conv2D(192,(1,1),activation='relu'))
+  model.add(GlobalAveragePooling2D())
+  model.add(Dense(5, activation='softmax'))
+  model.build(inputShape)
+  model.compile(loss=categorical_crossentropy,optimizer=Adam(0.001),metrics=['accuracy'])
+
+  return model
+
+def nin_cnn_c(inputShape):
+  model = Sequential()
+  model.add(Conv2D(32,kernel_size=(5,5),activation='relu',padding='valid'))
+  model.add(Conv2D(32,kernel_size=(5,5),activation='relu'))
+  model.add(Conv2D(32,kernel_size=(5,5),activation='relu'))
+  model.add(MaxPooling2D(pool_size=(3,3),strides=2))
+  model.add(Dropout(0.5))
+  model.add(Conv2D(64,(3,3),activation='relu',padding='same'))
+  model.add(Conv2D(64,(1,1),activation='relu',padding='same'))
+  model.add(Conv2D(64,(1,1),activation='relu',padding='same'))
+  model.add(MaxPooling2D(pool_size=(3,3),strides=2))
+  model.add(Dropout(0.5))
+  model.add(Conv2D(128,(3,3),activation='relu',padding='same'))
+  model.add(Conv2D(32,(1,1),activation='relu'))
+  model.add(Conv2D(5,(1,1)))
+  model.add(GlobalAveragePooling2D())
+  model.add(Flatten())
+  model.add(Dense(5, activation='softmax'))
+  model.build(inputShape)
+  
+  model.compile(loss=categorical_crossentropy,optimizer=Adam(0.001),metrics=['accuracy'])
+  return model
+
+def SimpleModel(inputShape):
+	model = tf.keras.models.Sequential([
+    keras.layers.Dense(512, activation='relu', input_shape = inputShape),
+    keras.layers.Dropout(0.2),
+    keras.layers.Dense(10)
+    ])
+
+	model.compile(optimizer='adam',
+                loss=tf.losses.SparseCategoricalCrossentropy(from_logits=True),
+                metrics=[tf.metrics.SparseCategoricalAccuracy()])
+
+	return model
